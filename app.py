cd9e6eacd7548227e1028041a5c5e7b6029716a2
Your name <omowunmidaud1@gmail.com>
2022-06-29 11:08:51 +0100

    connected to mysql database

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template, url_for, request
+from flaskext.mysql import MySQL
+import pymysql.cursors
 
 app = Flask(__name__)
+
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_DB'] = 'dictionary'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '@OPEyemi2001'
+
+mysql = MySQL(app, cursorclass=pymysql.cursors.DictCursor)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     user_response = ' '
     if request.method == 'POST':
-        user_response = request.form['word']
+        user_input = request.form['word']
+        conn = mysql.get_db()
+        cur = conn.cursor()
+        cur.execute('select meaning from word where word=%s', (user_input))
+        rv = cur.fetchall()
+        user_response = rv[0]['meaning']
+        
     return render_template('index.html', user_response=user_response)
 
 @app.route('/dashboard')
